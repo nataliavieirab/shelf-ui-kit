a6ce8388a8f1c83f9a68cb1b63fe87e57f7def48
Natalia Vieira <nataliabortolivieira@gmail.com>
2025-11-13 19:07:19 -0300

    configs adjusts for mode library

--- vite.config.ts
+++ vite.config.ts
@@ -25,6 +25,10 @@ export default defineConfig({
     }),
     dts({
       insertTypesEntry: true,
+      compilerOptions: {
+        noEmit: false,
+        skipLibCheck: true, // Ignora erros em bibliotecas
+      },
     }),
   ],
   resolve: {
