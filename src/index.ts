e49f2431f46eb7a36febb5e9ae3149d39c748e92
Natalia Vieira <nataliabortolivieira@gmail.com>
2025-11-15 16:44:42 -0300

    feat: export gradient icons components

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,29 @@
+import { HeightBoxSizes } from "./presentation/components/types";
+
 export * from "./theme_provider/create-custom-theme";
 export * from "./theme_provider/theme-provider";
 
-export * from "./presentation/components/CustomContainedButton/custom-contained-button";
-export * from "./presentation/components/CustomOutlinedButton/custom-outlined-button";
+export { default as CustomContainedButton } from "./presentation/components/CustomContainedButton/custom-contained-button";
+export { default as CustomOutlinedButton } from "./presentation/components/CustomOutlinedButton/custom-outlined-button";
+export { default as CustomDataTable } from "./presentation/components/CustomDataTable/custom-data-table";
 
-export * from "./presentation/components/CustomDataTable/custom-data-table";
+export { default as InputTextfield } from "./presentation/components/Textfields/InputTextfield/input-textfield";
+export { default as PasswordTextfield } from "./presentation/components/Textfields/PasswordTextfield/password-textfield";
+export { default as SearchTextfield } from "./presentation/components/Textfields/SearchTextfield/search-textfield";
+export { default as Select } from "./presentation/components/Select/select";
+export { default as Datepicker } from "./presentation/components/Datepicker/datepicker";
+export { default as CheckBoxWithLabel } from "./presentation/components/CheckBox/CheckBoxWithLabel/checkbox-w-label";
+export { default as CustomSwitch } from "./presentation/components/Switches/custom-switch";
+export { default as SwitchWithLabelExample } from "./presentation/components/Switches/switch-with-label-example";
 
-export * from "./presentation/components/Textfields/InputTextfield/input-textfield";
-export * from "./presentation/components/Textfields/PasswordTextfield/password-textfield";
-export * from "./presentation/components/Textfields/SearchTextfield/search-textfield";
-export * from "./presentation/components/Select/select";
-export * from "./presentation/components/Datepicker/datepicker";
-export * from "./presentation/components/CheckBox/CheckBoxWithLabel/checkbox-w-label";
-export * from "./presentation/components/Switches/custom-switch";
-export * from "./presentation/components/Switches/switch-with-label-example";
-
-export * from "./presentation/components/LayoutContainers/PageTitleContainer/page-title-container";
-export * from "./presentation/components/TabsContainer/tabs-container";
+export { default as PageTitleContainer } from "./presentation/components/LayoutContainers/PageTitleContainer/page-title-container";
+export { default as TabsContainer } from "./presentation/components/TabsContainer/tabs-container";
 export * from "./presentation/components/Sidebar/sidebar-context"; // Exporta o Contexto da Sidebar
 
-export * from "./presentation/components/ErrorBox/error-box";
-export * from "./presentation/components/NewImage/new-image";
-export * from "./presentation/components/Header/custom-header";
+export { default as ErrorBox } from "./presentation/components/ErrorBox/error-box";
+export { default as NewImage } from "./presentation/components/NewImage/new-image";
+export { default as CustomHeader } from "./presentation/components/Header/custom-header";
 
 export * from "./types/mui.d";
+export * from "./presentation/components/types";
+export * from "./presentation/components/Icons/gradient-icons";
